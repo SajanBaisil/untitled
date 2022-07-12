ad17a088c6faf71e9e1af2673683aa229782ff14
sajan <sajanbaisil12@gmail.com>
2022-07-12 10:29:11 +0530

    commit

---
+++ lib/src/multiplication.dart
@@ -0,0 +1,15 @@
+void main  (){
+  var num1 =10;
+  var num2 = 20, sum,diff,mul;
+
+  sum = num1 + num2;
+  diff = num2- num1;
+  mul = num1*num2;
+
+  print("sum is $sum");
+  print("difference is $diff");
+  print("the value is $mul");
+
+
+
+}
---
+++ lib/untitled.dart
@@ -0,0 +1,20 @@
+void main(){
+  String name = "Sajan Baisil";
+  int age = 22;
+  String email = "sajanbaisil12@gmail.com" ;
+  int phone = 88486653662;
+  String firstname = "Baisil";
+  String lastname = "francis";
+
+
+  print("My Name is ${name }");
+  print("I am ${age} yrs old");
+  print("My email ID is ${email}");
+  print("My phone no is ${phone}");
+  print(age>18);
+
+  print("${firstname +" " +lastname}");
+  print("My name is ${firstname += " Baisil"}");
+  print("My name is $firstname $lastname");
+
+}
